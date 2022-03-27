863a27abc7bda905e71c62ecc7496b22fef39ab0
CapNemo <46331608+Hone12345678@users.noreply.github.com>
2022-03-27 13:07:49 -0600

    building the basic setup of a MERN application with the nessecary pacakges imported

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path')
+const routes = require('./routes')
+const db = require('./config/connection');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes)
+
+db.once('open', () => {
+	app.listen(PORT, () => {
+		console.log(`API server running on port ${PORT}!`);
+	});
+});
