69f74d038f5f69e0d968d934a1ed48b0433f440a
lnguyen87 <luc_nguyen@outlook.com>
2022-03-30 17:19:16 -0600

    created nav functionality in header component

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,78 @@
-function Header() {
+import React from "react";
+
+
+function Header(props) {
+  const {
+    setCurrentComponent,
+  } = props; 
+
+
   return (
     <header>
-      <h1>WorthIt</h1>
+      <nav className="">
+        <ul className="">
+          <h1 className="">
+            <a
+              data-testid="link"
+              href="/worthIt"
+            >
+              Worth It
+            </a>
+          </h1>
+          <div className="">
+          <li className="">
+            <a
+              data-testid="about"
+              href="#about"
+              onClick={() => setCurrentComponent("about")}
+              className=""
+            >
+              About
+            </a>
+          </li>
+          <li className="">
+            <a
+              data-testid="contact"
+              href="#contact"
+              onClick={() => setCurrentComponent("contact")}
+              className=""
+            >
+              Contact
+            </a>
+          </li>
+          <li className="">
+            <a
+              data-testid="profile"
+              href="#profile"
+              onClick={() => setCurrentComponent("profile")}
+              className=""
+            >
+              Profile
+            </a>
+          </li>
+          <li className="">
+            <a
+              data-testid="gig"
+              href="#gig"
+              onClick={() => setCurrentComponent("gig")}
+              className=""
+            >
+              Gig
+            </a>
+          </li>
+          <li className="">
+            <a
+              data-testid="display"
+              href="#display"
+              onClick={() => setCurrentComponent("display")}
+              className=""
+            >
+              Display
+            </a>
+          </li>
+          </div>
+        </ul>
+      </nav>
     </header>
   );
 }
