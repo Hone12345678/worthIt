4ecd674e074aa6901eac9b0511bdf99a5acf5490
Ross Westwater <ross.westwater@gmail.com>
2022-03-30 19:48:05 -0600

    updated controller/routes. removed legacy code and old model file. added more commented code to uncomment for pword hashing and authentification.

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose')
+// const bcrypt = require('bcrypt')
 
 const gigsSchema = new Schema({
   name: {
@@ -10,24 +11,31 @@ const gigsSchema = new Schema({
 })
 
 const carSchema = new Schema({
+
   car: {
     type: String,
     allowNull: false,
     required: true,
     trim: true
   },
-  mph: {
+  mpg: {
     type: Number,
     allowNull: false,
     required: true,
     trim: true
   }
-})
+},
+{
+  toJSON: {
+    getters: true
+  }
+}
+)
 
 const userSchema = new Schema ({
   username: {
     type: String,
-    allowNull: false,
+    allowNull: true,
     required: true,
     unique: true,
     trim: true,
@@ -56,6 +64,21 @@ const userSchema = new Schema ({
   car: [carSchema],
   gigs: [gigsSchema]
 })
+
+// // set up pre-save middleware to create password
+// userSchema.pre('save', async function(next) {
+//   if (this.isNew || this.isModified('password')) {
+//     const saltRounds = 10;
+//     this.password = await bcrypt.hash(this.password, saltRounds);
+//   }
+
+//   next();
+// });
+
+// // compare the incoming password with the hashed password
+// userSchema.methods.isCorrectPassword = async function(password) {
+//   return bcrypt.compare(password, this.password);
+// };
 
 const User = model('user', userSchema)
 
