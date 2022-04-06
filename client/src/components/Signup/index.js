989bfbf4b9d575c31e2e987d435131dab9267c54
daveh <dave.halladay@outlook.com>
2022-04-05 18:43:49 -0600

    small changes to signup

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,8 +1,8 @@
 // allows a new user to create a new account and input there account information and basic settings
 
 import { Form, Button } from "react-bootstrap";
-import { useState } from "react";
 import AuthService from "../../utils/auth";
+import { useState } from "react";
 import { validateEmail } from "../../utils/helpers";
 
 function Signup(props) {
@@ -11,39 +11,7 @@ function Signup(props) {
     setLoginSelected
   } = props; 
 
-  const [formState, setFormState] = useState({ loginName: '', loginEmail: '', loginPassword: '' });
   const [errorMessage, setErrorMessage] = useState('');
-
-  const handleChange = (e) => {
-    console.log(e.target.name);
-    if (e.target.name === 'email') {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage('Your email is invalid.');
-      } else {
-        setErrorMessage('');
-      }
-    }
-    else if (e.target.name === 'password') {
-      console.log(e.target.value.length);
-      if (e.target.value.length < 8) {
-        setErrorMessage('Your password must be at least 8 characters in length.');
-      } else {
-        setErrorMessage('');
-      }
-    }
-    else if (!e.target.value.length){
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
-
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log('Handle Form', formState);
-    }
-  };
-
 
   const signupSubmit = (e) => {
     e.preventDefault();
@@ -52,9 +20,10 @@ function Signup(props) {
     const password = document.querySelector('#password').value.trim();
 
 
+
     if (username && email && password) {
       fetch('/api/users/', {
-        method: 'post', 
+        method: 'post',
         body: JSON.stringify({
           username,
           email,
@@ -62,22 +31,26 @@ function Signup(props) {
         }),
         headers: { 'Content-Type': 'application/json' }
       })
-      .then((res)=> { 
-        if(res.ok) {
+      .then((res)=> {
+        if (res.ok) {
           return res.json()
-        } else {
-          alert("There was an issue with your account credentials. Please ensure your email address is valid and your password is at least 8 characters long")
-          throw console.error(res);
         }
       })
       .then((res)=> {
-        console.log(res)
-        AuthService.login(res.token)
-        setLoginSelected(true);
-        setCurrentComponent("about")
-      }).catch(err => {
+        if (res?.token) {
+          setErrorMessage("");
+          AuthService.login(res.token)
+          setLoginSelected(true)
+          setCurrentComponent("profile")
+        }
+        else {
+          setErrorMessage("There was an issue with your account credentials. Please verify them and try again!")
+          // throw console.error(res);
+        }
+      })
+      .catch(err => {
         alert(err.message)
-    })
+      })
   }
 }
   
@@ -88,21 +61,16 @@ function Signup(props) {
 
       <Form.Group>
         <Form.Label htmlFor="username">Username: </Form.Label>
-        <Form.Control bsPrefix="neu-input"  id = "username" type="text" name="username" defaultValue={''} onBlur={handleChange}/>
+        <Form.Control bsPrefix="neu-input"  id = "username" type="text" name="username" defaultValue={''} />
       </Form.Group>
       <Form.Group>
         <Form.Label htmlFor="email">Email address: </Form.Label>
-        <Form.Control bsPrefix="neu-input" id = "email" type="email" name="email" defaultValue={''} onBlur={handleChange}/>
+        <Form.Control bsPrefix="neu-input" id = "email" type="email" name="email" defaultValue={''} />
       </Form.Group>
       <Form.Group>
         <Form.Label htmlFor="password">Password: </Form.Label>
-        <Form.Control bsPrefix="neu-input" id = "password" type="password" name="password" defaultValue={''} onBlur={handleChange}/>
+        <Form.Control bsPrefix="neu-input" id = "password" type="password" defaultValue={''} />
       </Form.Group>
-      {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
       <Button bsPrefix="neu-button" variant="primary" className="neu-button" type="submit">Submit</Button>
       <span>Already have an account? <u><a href="#login" onClick={() => setCurrentComponent("login")}>Login</a></u></span>
     </Form>
